ae9a053c420c1eb4687502e4c6bfe55ce2a6ce18
voje <voje@iskratel.si>
2023-02-17 11:54:41 +0100

    decode_test

--- internal/scribe/decode_svcacc_test.go
+++ internal/scribe/decode_svcacc_test.go
@@ -10,5 +10,10 @@ func TestDecode(t *testing.T) {
     assert.NoError(t, err)
 
     t.Logf("Result: %s", b)
+    assert.Equal(t, []byte("Hello, I'm a secret!\n"), b)
+
+    // This hangs for some reason
+    // _, err = decodeSvcaccJSON("./test_data/encoded.gpg", "wrongpass")
+    // assert.Error(t, err)
 }
 
